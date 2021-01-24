dccae3a94d5b27c996c8df4f83a3e0a8065589e1
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-24 20:59:39 +0200

    Adds 3.9 solution

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+
 morgan.token("body", (req, _res) => JSON.stringify(req.body))
+
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 )
