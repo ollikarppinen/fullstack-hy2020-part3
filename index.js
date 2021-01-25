8dcf078dfcaeed5f51a7ed7ba12a81d7923715b7
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-25 22:40:04 +0200

    Adds 3.18 solution

--- index.js
+++ index.js
@@ -17,20 +17,11 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 )
 
-const MAX_ID = 1000
-
-let persons = [
-  { id: 1, name: "Arto Hellas", number: "040-123456" },
-  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-  { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
-]
-
 app.get("/info", (request, response) => {
-  response.send(
-    `Phonebook has info for ${
-      persons.length
-    } people\n\n${new Date().toString()}`
+  Person.countDocuments().then((count) =>
+    response.send(
+      `Phonebook has info for ${count} people\n\n${new Date().toString()}`
+    )
   )
 })
 
